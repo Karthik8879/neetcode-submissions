beee95bf73c80f9b51f69d32da995363e0f0cadb
Karthik Sridhar <146142934+Karthik8879@users.noreply.github.com>
2026-09-18 06:21:55 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.java
@@ -0,0 +1,63 @@
+class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int low = i+1;
+            int high = nums.length-1;
+            while(low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[low]);
+                    temp.add(nums[high]);
+                    res.add(temp);
+                    while(low < high && nums[low] == nums[low+1]) low++;
+                    while(low < high && nums[high] == nums[high-1]) high--;
+                    low++;
+                    high--;
+                } else if(sum < 0) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            int low = i+1;
+            int high = nums.length-1;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            while(low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[low]);
+                    temp.add(nums[high]);
+                    // if(!res.contains(temp)) {
+                        res.add(temp);
+                    // }
+                    while(low < high && nums[low] == nums[low+1]) low++;
+                    while(low < high && nums[high] == nums[high-1]) high--;
+                    low++;
+                    high--;
+                } else if(sum < 0) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+        }
+        return res;
+    }
+}
